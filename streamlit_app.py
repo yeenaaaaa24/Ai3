32ff53ef9374da9c11c3fbd22af64b9c689781d5
yeenaaaaa24 <20242915@sdhs.gwe.hs.kr>
2024-12-13 19:44:26 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,8 @@ import gdown
 file_id = '1e-9_UjEFUKaybfkBS4A7egHhh3rvCodU'
 
 # Google Drive에서 파일 다운로드 함수
-@st.cache(allow_output_mutation=True)
+#@st.cache(allow_output_mutation=True)
+@st.cache_resource
 def load_model_from_drive(file_id):
     url = f'https://drive.google.com/uc?id={file_id}'
     output = 'model.pkl'
@@ -22,7 +23,7 @@ def load_model_from_drive(file_id):
 def display_left_content(image, prediction, probs, labels):
     st.write("### 왼쪽: 기존 출력 결과")
     if image is not None:
-        st.image(image, caption="업로드된 이미지", use_column_width=True)
+        st.image(image, caption="업로드된 이미지", use_container_width=True)
     st.write(f"예측된 클래스: {prediction}")
     st.markdown("<h4>클래스별 확률:</h4>", unsafe_allow_html=True)
     for label, prob in zip(labels, probs):
@@ -43,7 +44,7 @@ def display_right_content(prediction, data):
     # 1st Row - Images
     for i in range(3):
         with cols[i]:
-            st.image(data['images'][i], caption=f"이미지: {prediction}", use_column_width=True)
+            st.image(data['images'][i], caption=f"이미지: {prediction}", use_container_width=True)
     # 2nd Row - YouTube Videos
     for i in range(3):
         with cols[i]:

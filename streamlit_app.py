e3b75fb36c882d1fabe54ecf9514b8f32a07e5dd
yeenaaaaa24 <20242915@sdhs.gwe.hs.kr>
2024-12-13 19:56:48 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,7 @@ def load_model_from_drive(file_id):
 def display_left_content(image, prediction, probs, labels):
     st.write("### 왼쪽: 기존 출력 결과")
     if image is not None:
-        st.image(image, caption="업로드된 이미지", use_container_width=True)
+        st.image(image, caption="업로드된 이미지", use_column_width=True)
     st.write(f"예측된 클래스: {prediction}")
     st.markdown("<h4>클래스별 확률:</h4>", unsafe_allow_html=True)
     for label, prob in zip(labels, probs):
